3705af62325d823dcc3cde7840b39f22230eb1f3
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-07-24 13:29:35 +0530

    Create main.tf

---
+++ sns/main.tf
@@ -0,0 +1,10 @@
+resource "aws_sns_topic" "sns" {
+  name = var.topic_name
+}
+  
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.sns.arn
+  protocol  = "email"
+  endpoint  = var.email_address  
+}
